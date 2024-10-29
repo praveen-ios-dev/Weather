bff6d40da88fe5867e866decd17aab1b0cdbfb3c
Kumar praveen <kumar.praveen@mindmaze.ch>
2024-10-29 23:58:59 +0530

    Initial Commit

---
+++ sampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sampleApp
+//
+//  Created by Arohi on 29/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sampleApp/sampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleAppApp.swift
+//  sampleApp
+//
+//  Created by Arohi on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
